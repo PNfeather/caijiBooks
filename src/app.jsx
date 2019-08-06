4ca40bc8e915f815bb0b7d764b00a13bfdd6f957
PNFeather <921192114@qq.com>
2019-08-06 18:50:27 +0800

    go

--- src/app.jsx
+++ src/app.jsx
@@ -22,12 +22,14 @@ class App extends Component {
 
   config = {
     pages: [
-      'pages/donateBook/index',
 
       'pages/login/index',
       'pages/index/index',
       'pages/bookList/index',
-      // 'pages/donateBook/index'
+      'pages/borrowBook/index',
+      'pages/repayBook/index',
+      'pages/donateBook/index',
+      'pages/removeBook/index',
     ],
     window: {
       backgroundTextStyle: 'light',
