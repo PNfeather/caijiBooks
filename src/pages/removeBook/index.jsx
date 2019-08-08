fc50607606bb94785b5b8bdd3dca3fd86210a9c0
PNFeather <921192114@qq.com>
2019-08-08 16:03:43 +0800

    go

--- src/pages/removeBook/index.jsx
+++ src/pages/removeBook/index.jsx
@@ -77,6 +77,7 @@ export default class Index extends Component {
         const reset = {
           donateName: '',
           donateTime: '',
+          donateType: '',
           borrowName: '',
           borrowTime: ''
         }
